a42b9803a0255be2421a4239eeb1c322071cfc07
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2024-04-17 13:49:46 -0400

    Create Solution.js

---
+++ A contribution by Lisa-Has-Ideas/Solution.js
@@ -0,0 +1,78 @@
+var inputs = readline().split(' ');
+const n = parseInt(inputs[0]);
+console.log("+---+");
+if(n == 0) {
+    console.log("|   |")
+    console.log("|   |")
+    console.log("|   |")
+}
+else if(n == 1) {
+    console.log("|   |")
+    console.log("| * |")
+    console.log("|   |")
+}
+else if(n == 2) {
+    console.log("|*  |")
+    console.log("|   |")
+    console.log("|  *|")
+}
+else if(n == 3) {
+    console.log("|*  |")
+    console.log("| * |")
+    console.log("|  *|")
+}
+else if(n == 4) {
+    console.log("|* *|")
+    console.log("|   |")
+    console.log("|* *|")
+}
+else if(n == 5) {
+    console.log("|* *|")
+    console.log("| * |")
+    console.log("|* *|")
+}
+else if(n == 6) {
+    console.log("|* *|")
+    console.log("|* *|")
+    console.log("|* *|")
+}
+console.log("+---+");
+
+const k = parseInt(inputs[1]);
+
+if(k == 0) {
+    console.log("|   |")
+    console.log("|   |")
+    console.log("|   |")
+}
+else if(k == 1) {
+    console.log("|   |")
+    console.log("| * |")
+    console.log("|   |")
+}
+else if(k == 2) {
+    console.log("|*  |")
+    console.log("|   |")
+    console.log("|  *|")
+}
+else if(k == 3) {
+    console.log("|*  |")
+    console.log("| * |")
+    console.log("|  *|")
+}
+else if(k == 4) {
+    console.log("|* *|")
+    console.log("|   |")
+    console.log("|* *|")
+}
+else if(k == 5) {
+    console.log("|* *|")
+    console.log("| * |")
+    console.log("|* *|")
+}
+else if(k == 6) {
+    console.log("|* *|")
+    console.log("|* *|")
+    console.log("|* *|")
+}
+console.log("+---+");
